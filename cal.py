51a88dab576c7b5ebe519fe08064a3166157c966
swastik7777 <swastikgomase.sae.comp@gmail.com>
2026-09-20 11:49:17 +0530

    Addded the calculator program

---
+++ cal.py
@@ -0,0 +1,7 @@
+#Hello this is calculator program
+
+num1 = int(input("Enter the first number1: "))
+
+num2 = int(input("Enter the second number2: "))
+
+print(f"The sum of two number is {num1 + num2}")
